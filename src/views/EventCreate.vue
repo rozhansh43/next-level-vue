c99eb2312e1f0d3119db4089553c9ab87931a96e
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-14 03:37:54 -0700

    init

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -7,7 +7,7 @@
         <option v-for="cat in categories" :key="cat">{{ cat }}</option>
       </select>
 
-      <h3>Name & describe your event</h3>
+      <h3>Name describe your event</h3>
       <div class="field">
         <label>Title</label>
         <input v-model="event.title" type="text" placeholder="Add an event title"/>
